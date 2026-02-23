2d1f13629f689204dda6f823304dc05fc0b8b9fd
Abdullah Al Fahim <fahim.devx@gmail.com>
2026-02-23 12:32:22 +0600

    payment structure deleted

--- src/modules/payment/stripe/stripe.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { StripePayment } from '../../../common/lib/Payment/stripe/StripePayment';
-
-@Injectable()
-export class StripeService {
-  async handleWebhook(rawBody: string, sig: string | string[]) {
-    return StripePayment.handleWebhook(rawBody, sig);
-  }
-}
